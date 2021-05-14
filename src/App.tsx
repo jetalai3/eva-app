179ef98dafb327d66716548a91959233e2ac4a4d
Viktar Svintsitski <jetalai3@gmail.com>
2021-05-14 18:15:24 +0300

    init: start the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      Eve app would be here
+    </div>
+  );
+}
+
+export default App;
